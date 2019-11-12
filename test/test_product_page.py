017d966465d33f862d7b3cb873bf029c9ae84756
Wolf778 <7473747@mail.ru>
2019-11-12 07:07:17 +0300

    Что то сделали, пока не понятно

--- test/test_product_page.py
+++ test/test_product_page.py
@@ -6,8 +6,8 @@ from selenium.common.exceptions import NoAlertPresentException
 def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = Product(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
+    page.open()                     # открываем страницу
+    page.add_product_to_rec()       # добавляем товар в корзину
 
 
 def solve_quiz_and_get_code(self):
